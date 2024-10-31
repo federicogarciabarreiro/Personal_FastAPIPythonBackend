4fe0ce36d62a62a65ff234ab981385fdfa20941b
federicogarciabarreiro <federicogarciabarreiro@hotmail.com>
2024-10-31 11:55:17 +0100

    Update
    Se autoriza el dominio de itch.io en el backend.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import router as api_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://html-classic.itch.zone"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", include_in_schema=False)
 async def root():
